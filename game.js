1346bc61f19366a7b916c52f33e4d0050e056eb7
uroojahamad <uroojahamad786@gmail.com>
2021-06-26 15:22:01 +0530

    create function to roll the dice

--- game.js
+++ game.js
@@ -1,19 +1,17 @@
 console.log("Snake and Ladder");
 const number = [
     [16, 15, 14, 13],
-    [9, 10, 11, 12], 
-    [8, 7, 6, 5], 
-    [1, 2, 3, 4] 
+    [9, 10, 11, 12],
+    [8, 7, 6, 5],
+    [1, 2, 3, 4]
 ];
 
-function gameBoard(){
+function gameBoard() {
     let str = "";
 
     for (let i = 0; i < number.length; i++) {
-        console.log(i, number[i]);
         str += `<div class="board">`
         for (let num of number[i]) {
-            console.log(num);
             str += `
                 <div class="box-number">
                     <span>${num}</span>
@@ -22,6 +20,17 @@ function gameBoard(){
         }
         str += `</div>`
     }
-    console.log(str);
     document.querySelector(".container-board").innerHTML = str;
+}
+
+//Roll Dice 
+const image = document.querySelector("img");
+function rollDice() {
+    image.setAttribute("src", "images/dice.gif");
+    setTimeout(rollOut, 300);
+}
+
+function rollOut() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    image.setAttribute("src", `images/${randomNumber}.png`);
 }
